b669a6f53bd631aaab297322d47941e08d3583e8
万雅虎 <vipwan@outlook.com>
2023-11-05 21:14:21 +0800

    demo

--- Biwen.AutoClassGen.TestConsole/Classes.cs
+++ Biwen.AutoClassGen.TestConsole/Classes.cs
@@ -1,18 +1,21 @@
 ﻿using Biwen.AutoClassGen.TestConsole.Interfaces;
 
-namespace Biwen.AutoClassGen.Models;
-
-/// <summary>
-/// 分页请求
-/// </summary>
-[AutoGen("QueryRequest", "Biwen.AutoClassGen.Models")]
-[AutoGen("Query2Request", "Biwen.AutoClassGen.Models")]
-public interface IQueryRequest : IPager, IQuery
+namespace Biwen.AutoClassGen.Models
 {
-}
 
-[AutoGen("TenantRealRequest", "Biwen.AutoClassGen.Models")]
-public interface ITenantRealRequest : ITenantRequest
-{
+    /// <summary>
+    /// 分页请求
+    /// </summary>
+    [AutoGen("QueryRequest", "Biwen.AutoClassGen.Models")]
+    [AutoGen("Query2Request", "Biwen.AutoClassGen.Models")]
+    public interface IQueryRequest : IPager, IQuery
+    {
+    }
+
+    [AutoGen("TenantRealRequest", "Biwen.AutoClassGen.Models")]
+    public interface ITenantRealRequest : ITenantRequest
+    {
+
+    }
 
 }
