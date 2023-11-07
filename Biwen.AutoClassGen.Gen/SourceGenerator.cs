3e8a0a63398884575ec0bb6d3e1dfa7943b8c450
万雅虎 <vipwan@outlook.com>
2023-11-07 14:03:06 +0800

    code cleanup

--- Biwen.AutoClassGen.Gen/SourceGenerator.cs
+++ Biwen.AutoClassGen.Gen/SourceGenerator.cs
@@ -4,7 +4,6 @@
 
 namespace Biwen.AutoClassGen
 {
-    using System;
     using System.Collections.Generic;
     using System.Collections.Immutable;
     using System.Linq;
--- Biwen.AutoClassGen.TestConsole/Classes.cs
+++ Biwen.AutoClassGen.TestConsole/Classes.cs
@@ -1,19 +1,19 @@
 ﻿using Biwen.AutoClassGen.TestConsole.Interfaces;
 
-namespace Biwen.AutoClassGen.Models;
-
-
-/// <summary>
-/// 分页请求
-/// </summary>
-[AutoGen("QueryRequest", "Biwen.AutoClassGen.Models")]
-[AutoGen("Query2Request", "Biwen.AutoClassGen.Models")]
-public interface IQueryRequest : IPager, IQuery
+namespace Biwen.AutoClassGen.Models
 {
-}
+    /// <summary>
+    /// 分页请求
+    /// </summary>
+    [AutoGen("QueryRequest", "Biwen.AutoClassGen.Models")]
+    [AutoGen("Query2Request", "Biwen.AutoClassGen.Models")]
+    public interface IQueryRequest : IPager, IQuery
+    {
+    }
 
-[AutoGen("TenantRealRequest", "Biwen.AutoClassGen.Models")]
-public interface ITenantRealRequest : ITenantRequest
-{
+    [AutoGen("TenantRealRequest", "Biwen.AutoClassGen.Models")]
+    public interface ITenantRealRequest : ITenantRequest
+    {
 
+    }
 }
